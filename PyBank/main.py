244c9fcdc52838776e0a3542a29e8c883d66162f
jahnferdinandus <jahnferdinandus@gmail.com>
2023-09-09 16:04:11 -0400

    First Attempt

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,105 +2,63 @@
 import os
 import csv
 
-csv_file = "Module-3-Challenge---Python\\PyBank\\Resources\\budget_data.csv"
+CSV_PATH = os.path.join("Resources", "budget_data.csv")
+OUTPUT_PATH = os.path.join("analysis","analysis.txt")
+MONTH_IND = 0
+PROFIT_IND = 1
 
-# Month count code
+# Total Months
 month_count = 0
-with open (csv_file) as budget_data:
-    csv_reader = csv.reader(budget_data, delimiter=',')
-    for month in csv_reader:
+total_profit = 0
+prev_profit = None #Lines to intiate the tracking of these variables
+total_change = 0
+max_increase = float('-inf') #initialized with negative infinity to make sure that any posotive values enountered in the code are higher than the initial value
+max_decrease = float('inf') #initialized with  infinity to make sure that any negative values enountered in the code are lower than the initial value
+print(__file__)
+
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
+with open(CSV_PATH) as budget_data:
+    csv_reader = csv.reader(budget_data)
+    next(csv_reader) #skipped the header row
+    #Total Profit/Loss
+    for row in csv_reader:
         month_count += 1
-
-print("Total Months:", month_count)
-
-# Total value calculation code
-column_index = 1 #defined the column index
-total = 0
-with open (csv_file, "r") as budget_data:
-    csv_reader = csv.reader(budget_data, delimiter=',')
-    next(csv_reader) #skipped the header row
-    for row in csv_reader:
-        value = float(row[column_index])
-        total += value
-total_value_int = int(total) #removed decimals by converting to int. 
-
-print("Total", total_value_int)
-
-# Average change calculation code
-
-column_index = 1 #defined the column index
-with open (csv_file, "r") as budget_data:
-    csv_reader = csv.reader(budget_data, delimiter=',')
-    next(csv_reader) #skipped the header row
-
-    prev_value = None #Lines to intiate the tracking of these variables
-    total_change = 0
-    row_count = 0
-
-    for row in csv_reader:
-        value = float(row[column_index]) #this line is used to extract the value from the column index and convert it to a float
-        if prev_value is not None : # this line is used to skip the first line of row which does not have a previos value and track the change of subsequent rows
-            change = value - prev_value
+        date = row[MONTH_IND]
+        current_profit = int(row[PROFIT_IND])
+        total_profit += current_profit
+        
+        #Profit Change
+        if prev_profit is not None : # this line is used to skip the first line of row which does not have a previos value and track the change of subsequent rows
+            change = current_profit - prev_profit
             total_change += change
-            row_count += 1
-
-        prev_value = value # this line is used to update the previous value 
-    if row_count > 0: #line added to make the answer an int instead of text. Why cannot I simply remove this line. 
-        average_change = total_change / row_count #average change calculation
-        print  ("Average Change : $",average_change)
-
-# Max increase calculation code
-
-column_index = 1 #defined the profit column index
-with open (csv_file, "r") as budget_data:
-    csv_reader = csv.reader(budget_data, delimiter=',')
-    next(csv_reader) #skipped the header row
-   
-    prev_value = None #Lines to intiate the tracking of these variables
-    max_increase = float('-inf') #initialized with negative infinity to make sure that any posotive values enountered in the code are higher than the initial value
-    max_increase_date = ""
-    
-    for row in csv_reader:
-        date = row[0]
-        value = float(row[column_index])
-
-        if prev_value is not None:
-            change = value - prev_value
+            #Max Increase
             if change > max_increase:
                 max_increase = change
                 max_increase_date = date
-
-        prev_value = value
-
-    if max_increase_date:
-        print("Greatest Increase in Profits:", max_increase_date, "($", max_increase, ")")
-
-
-# Max decrease calculation code
-
-column_index = 1 #defined the profit column index
-with open (csv_file, "r") as budget_data:
-    csv_reader = csv.reader(budget_data, delimiter=',')
-    next(csv_reader) #skipped the header row
-   
-    prev_value = None #Lines to intiate the tracking of these variables
-    max_decrease = float('inf') #initialized with  infinity to make sure that any negative values enountered in the code are lower than the initial value
-    max_decrease_date = ""
-    
-    for row in csv_reader:
-        date = row[0]
-        value = float(row[column_index])
-
-        if prev_value is not None:
-            change = value - prev_value
+            #Max Decrease
             if change < max_decrease:
                 max_decrease = change
                 max_decrease_date = date
+        prev_profit = current_profit # this line is used to update the previous value 
 
-        prev_value = value
+#Average Change
+average_change = total_change / (month_count-1) #average change calculation
 
-    if max_decrease_date:
-        print("Greatest Decrease in Profits:", max_decrease_date, "($", max_decrease, ")")
+#Formatting
+formatted_total = "${:.0f}".format(total_profit)
+formatted_average_change = "${:.2f}".format(average_change)
+formatted_max_increase = "${:.0f}".format(max_increase)
+formatted_max_decrease = "${:.0f}".format(max_decrease)
 
+#Text Output
+output_txt = (
+    f"Total Months: {month_count}\n" 
+    f"Total {formatted_total}\n"
+    f"Average Change : {formatted_average_change}\n"
+    f"Greatest Increase in Profits: {max_increase_date} ({formatted_max_increase})\n"
+    f"Greatest Decrease in Profits: {max_decrease_date} ({formatted_max_decrease})\n"
+)
 
-
+with open(OUTPUT_PATH, "w") as output_file:
+    output_file.write(output_txt)
+print(output_txt)
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -2,25 +2,24 @@
 import os
 import csv
 
-csv_file = "Module-3-Challenge---Python\\PyPoll\\Resources\\election_data.csv"
+CSV_PATH = os.path.join("Resources", "election_data.csv")
+OUTPUT_PATH = os.path.join("analysis", "analysis.txt")
 
-# Vote count code
+# Total Vote Count
+
 vote_count = 0
-with open (csv_file) as election_data:
-    csv_reader = csv.reader(election_data, delimiter=',')
-    for vote in csv_reader:
-        vote_count += 1
-
-print("Total Votes:", vote_count)
-
-# List of candidates
-
 candidate_votes = {}
 total_votes= 0
-with open (csv_file) as election_data:
-    csv_reader = csv.reader(election_data, delimiter=',')
+print(__file__)
+
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
+with open (CSV_PATH) as election_data:
+    csv_reader = csv.reader(election_data)
     next(csv_reader) #skipped the header row
     for row in csv_reader:
+        vote_count += 1
+
+        # The List of Candidates
         total_votes += 1
         candidate_name = row[2]
         if candidate_name in candidate_votes:
@@ -30,9 +29,31 @@ with open (csv_file) as election_data:
 
 percentage_votes = {candidate:(votes/total_votes * 100) for candidate, votes in candidate_votes.items()}
 
+# The Winner
+
 winner = max(candidate_votes, key=candidate_votes.get)
 
+#Text Output
+results = []
+results.append("Total Votes: " + str(vote_count))  # Added vote count
+results.append("Election Results")
+results.append("-------------------------")
+results.append(f"Total Votes: {total_votes}")
+results.append("-------------------------")
+for candidate, votes in candidate_votes.items():
+    percentage = percentage_votes[candidate]
+    results.append(f"{candidate}: {percentage:.3f}% ({votes})")
+results.append("-------------------------")
+results.append(f"Winner: {winner}")
+results.append("-------------------------")
+
+
+
+with open(OUTPUT_PATH, "w") as output_file:
+    output_file.write('\n'.join(results))
+
 # Print the results
+print("Total Votes:", vote_count)
 print("Election Results")
 print("-------------------------")
 print(f"Total Votes: {total_votes}")
